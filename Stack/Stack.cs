b71f895590e9a4ef2c81271b357d09fff77b3d5c
Sivakumar K R <seevaathome1@gmail.com>
2015-03-15 17:19:21 +0530

    Stack Complete

---
+++ Stack/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Stack
+{
+	public class Stack<T> {
+		private class Node<T> {
+			public T data;
+			public Node<T> next;
+		}
+		private Node<T> head;
+
+		public void Push (T val) {
+			if (head == null) {
+				head = new Node<T> (){ data = val };
+			} else {
+				Node<T> temp = new Node<T> (){ data = val };
+				temp.next = head;
+				head = temp;
+			}
+		}
+		public void Pop () {
+			if (head == null)
+				return;
+			head = head.next;
+		}
+
+		public T Top () {
+			if (head == null)
+				return default(T);
+			return head.data;
+		}
+	}
+}
+
---
+++ Stack/StkTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Stack
+{
+	public class StkTest {
+		static void Main () {
+			Stack<int> stack = new Stack<int> ();
+			int[] a = new int[]{ 1, 2, 3, 4, 5 };
+			foreach (int v in a) {
+				stack.Push (v);
+			}
+
+			Console.WriteLine (stack.Top ());
+			foreach (int v in a)
+				stack.Pop ();
+			Console.WriteLine (stack.Top ());
+
+		}
+	}
+}
+
