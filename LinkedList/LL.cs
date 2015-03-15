a2e84620099a6bac1b4cfefd1e20162b945d4fee
Sivakumar K R <seevaathome1@gmail.com>
2015-03-15 15:10:00 +0530

    LinkedList Complete

---
+++ LinkedList/LL.cs
@@ -0,0 +1,93 @@
+﻿using System;
+namespace LinkedList
+{
+	public class LL<T> {
+
+		public int Count { get; set; }
+
+		public class Node<T> {
+			public Node<T> next;
+			public T data;
+		}
+		Node<T> head;
+
+		public Node<T> Last {
+
+			get {
+				Node<T> temp = head;
+				while (temp.next != null)
+					temp = temp.next;
+				return temp;
+			}
+		}
+		public void Add (T val) {
+			if (head == null) {
+				head = new Node<T> ();
+				head.data = val;
+			} else {
+
+				Node<T> temp = Last;
+				temp.next = new Node<T> ();
+				temp.next.data = val;
+			}
+			Count = Count + 1;
+		}
+
+		public void Insert (T val, int Position) {
+			if (Position > Count || Position < 0)
+				return;
+
+			Node<T> newNode = new Node<T> () {
+				data = val
+			};
+
+			if (Position == 0) {
+				newNode.next = head;
+				head = newNode;
+
+			} else if (Position == Count) {
+				newNode.data = val;
+				Last.next = newNode;
+			} else {
+				Node<T> temp = head;
+				int i = 1;
+				while (temp != null && i < Position) {
+					temp = temp.next;
+					i++;
+				}
+
+				newNode.next = temp.next;
+				temp.next = newNode;
+			}
+			Count = Count + 1;
+		}
+
+		public void Delete (int Position) {
+			if (Position >= Count || Position < 0)
+				return;
+
+			if (Position == 0)
+				head = head.next;
+			else {
+				Node<T> temp = head;
+				int i = 1;
+				while (temp != null && i < Position) {
+					temp = temp.next;
+					i++;
+				}
+
+				temp.next = temp.next.next;
+			}
+			Count = Count - 1;
+		}
+
+		public void Display () {
+			Node<T> temp = head;
+			while (temp != null) {
+				Console.WriteLine (temp.data);
+				temp = temp.next;
+			}
+		}
+	}
+}
+
---
+++ LinkedList/LinkedListTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace LinkedList
+{
+	public class LinkedListTest {
+		static void Main () {
+			LL<int> list = new LL<int> ();
+			int[] a = new int[]{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+			foreach (var v in a) {
+				list.Add (v);
+			}
+			list.Display ();
+
+			list.Insert (100, 1);
+			list.Insert (200, 1);
+			list.Insert (100, 10);
+			list.Insert (100, 5);
+
+			Console.WriteLine ();
+			list.Display ();
+
+			list.Delete (0);
+			list.Delete (0);
+			list.Delete (0);
+			list.Delete (2);
+			list.Delete (list.Count - 1);
+			Console.WriteLine ();
+			list.Display ();
+		}
+	}
+}
+
