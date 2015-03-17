781e1963cd0e6f470eeaecc62778f66c874c7ac4
Sivakumar K R <seevaathome1@gmail.com>
2015-03-17 21:37:03 +0530

    Trie Complete

--- Trie/Trie.cs
+++ Trie/Trie.cs
@@ -2,13 +2,78 @@
 using System.Security.Cryptography;
 
 
-namespace DataStructures {
+namespace DataStructures
+{
 
 	public class Trie {
 		const int Size = 256;
 		private class Node {
 			public bool end { get; set; }
 			public Node[] children = new Node[Size];
+		}
+		Node root;
+		public Trie () {
+			root = new Node ();
+		}
+		private void InsertUtil (Node temp, string word) {
+			for (int i = 0; i < word.Length; i++) {
+				if (temp.children[(int)word[i]] == null)
+					temp.children[(int)word[i]] = new Node ();
+				temp = temp.children[(int)word[i]];
+			}
+			temp.end = true;
+		}
+		public void Insert (string word) {
+			if (String.IsNullOrWhiteSpace (word))
+				return;
+			InsertUtil (root, word);
+		}
+
+
+		private bool IsEmptyNode (Node temp) {
+			for (int i = 0; i < Size; i++)
+				if (temp.children[i] != null)
+					return false;
+			return true;
+		}
+		private bool DeleteUtil (Node temp, string word, int i) {
+
+			if (temp.end && i == word.Length) {
+				temp.end = false;
+				return true;
+			}
+
+			bool flag = false;
+			if (i < word.Length && temp.children[(int)word[i]] != null)
+				flag = DeleteUtil (temp.children[(int)word[i]], word, i + 1);
+
+			if (flag) {
+				if (IsEmptyNode (temp.children[(int)word[i]]) && !temp.end) {
+					temp.children[(int)word[i]] = null;
+					return true;
+				}
+			}
+			return false;
+		}
+		public void Delete (string word) {
+			if (String.IsNullOrWhiteSpace (word))
+				return;
+			DeleteUtil (root, word, 0);
+		}
+
+		private void DisplayUtil (Node temp, string buffer) {
+			if (temp.end) {
+				Console.WriteLine (buffer);
+			}
+			for (int i = 0; i < Size; i++) {
+				if (temp.children[i] == null)
+					continue;
+				DisplayUtil (temp.children[i], buffer + (char)i);
+			}
+
+		}
+		public void Display () {
+			DisplayUtil (root, "");
 		}
 	}
 }
--- Trie/TrieTest.cs
+++ Trie/TrieTest.cs
@@ -3,6 +3,20 @@ namespace DataStructures
 {
 	public class TrieTest {
 		static void Main () {
+			Trie trie = new Trie ();
+			string[] strs = new string[] {"siva", "sivaranjani", "gokul", "ganesh", "g", "balaji", "prem", "sairaj", "raja", "prakash",
+				"chinna", "varadan", "sathya", "vignesh", "ram", "vinoth", "siva sah"
+			};
+			foreach (var v in strs)
+				trie.Insert (v);
+			trie.Display ();
+
+			trie.Delete (strs[0]);
+			trie.Display ();
+			trie.Delete (strs[1]);
+			trie.Delete (strs[2]);
+			trie.Delete (strs[3]);
+			trie.Display ();
 
 		}
 	}
